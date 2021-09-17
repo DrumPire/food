0c4a77f3b9691ae9b0c82b33edc9f38625a89ec7
DrumPire <expresproject@gmail.com>
2021-09-17 16:41:08 +0300

    add timer

--- js/test.js
+++ js/test.js
@@ -1,32 +1,32 @@
-const btns = document.querySelectorAll('button'),
-      wrapper = document.querySelector('.btn-block');
+// const btns = document.querySelectorAll('button'),
+//       wrapper = document.querySelector('.btn-block');
 
-btns[0].addEventListener('click', () => {
-  // if (!btns[1].classList.contains('red')) {
-  //   btns[1].classList.add('red');
-  // } else {
-  //   btns[1].classList.remove('red');
-  // }
+// btns[0].addEventListener('click', () => {
+//   // if (!btns[1].classList.contains('red')) {
+//   //   btns[1].classList.add('red');
+//   // } else {
+//   //   btns[1].classList.remove('red');
+//   // }
 
-  btns[1].classList.toggle('red');
-});
-
-// console.log(btns[0].className);
-
-wrapper.addEventListener('click', e => {
-  const target = e.target;
-  if(target && target.matches('button.red')) {
-    console.log(target);
-  }
-});
-// btns.forEach(btn => {
-//   btn.addEventListener('click', () => {
-//     console.log('click');
-//   });
+//   btns[1].classList.toggle('red');
 // });
 
+// // console.log(btns[0].className);
 
-const btn = document.createElement('button');
+// wrapper.addEventListener('click', e => {
+//   const target = e.target;
+//   if(target && target.matches('button.red')) {
+//     console.log(target);
+//   }
+// });
+// // btns.forEach(btn => {
+// //   btn.addEventListener('click', () => {
+// //     console.log('click');
+// //   });
+// // });
 
-btn.classList.add('red');
-wrapper.append(btn);
+
+// const btn = document.createElement('button');
+
+// btn.classList.add('red');
+// wrapper.append(btn);
